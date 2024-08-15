b7b583a71a7dda911c66317cb0b7bfa7e36d0279
Miguel Vargas <miguel.vargas.work@outlook.com>
2024-08-15 18:30:33 -0500

    Basic types TS

--- src/typescript/BasicTypes.tsx
+++ src/typescript/BasicTypes.tsx
@@ -1,9 +1,13 @@
 export const BasicTypes = () => {
-    const name = 'Miguel'
+    const name: string = 'Miguel';
+    const names: string[] = ['Miguel', 'Angel', 'Perez'];
+    const numbers: number[] = [1, 2, 3, 4, 5];
     return (
         <>
             <h1>Basic Types</h1>
             <h2>{name}</h2>
+            <h2>{names.join(', ')}</h2>
+            <h2>{numbers.join(', ')}</h2>
         </>
     )
 }
