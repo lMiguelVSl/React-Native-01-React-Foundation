f6a2df264869e7c1af75b69e0930239bb4f9d223
Miguel Vargas <miguel.vargas.work@outlook.com>
2024-08-22 19:13:28 -0500

    Code basic refactored

--- src/components/UsersPage..tsx
+++ src/components/UsersPage..tsx
@@ -2,13 +2,20 @@ import axios from "axios";
 import { useEffect } from "react"
 import { ReqResUserResponse } from "../interfaces";
 
+const loadUsers = async () => {
+    try {
+        const { data } = await axios.get<ReqResUserResponse>('https://reqres.in/api/users?page=2');
+        return data.data;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+};
+
 export const UsersPage = () => {
 
     useEffect(() => {
-        axios.get<ReqResUserResponse>('https://reqres.in/api/users?page=2')
-        .then(res => {
-            console.log(res.data.data[0]);
-        });
+        loadUsers().then(users => console.log(users));
     }, []);
 
     return (
